e6e9686d828de36eca66ae48fabd05dcd202088e
John R. McGarvey <johnmcgarvey55@gmail.com>
2023-05-24 01:08:40 -0400

    handle case if there is more than one page for a slug

--- index.mjs
+++ index.mjs
@@ -1,28 +1,34 @@
-import { readFileSync, writeFileSync } from 'fs'
-import { NodeHtmlMarkdown } from 'node-html-markdown'
-const  nhm =  new NodeHtmlMarkdown()
+import { readFileSync, writeFileSync } from "fs";
+import { NodeHtmlMarkdown } from "node-html-markdown";
+const nhm = new NodeHtmlMarkdown();
 
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 
 const getMD = async (slug) => {
-    const pagedata = await fetch("https://learn.codethedream.org/wp-json/wp/v2/pages?" + "slug=" + slug)
-    const pagejsonarray = await pagedata.json()
-    const pagejson = pagejsonarray[0]
-    const md = nhm.translate("<h1>" + pagejson.title.rendered + "</h1>\n" + pagejson.content.rendered)
-    const title = pagejson.title.rendered
-    const titleYaml = "\"" + title + "\""
-    const description = "\"imported from WordPress," + title + "\""
+  const pagedata = await fetch(
+    "https://learn.codethedream.org/wp-json/wp/v2/pages?" + "slug=" + slug
+  );
+  const pagejsonarray = await pagedata.json();
+  pagejsonarray.forEach((pagejson, i) => {
+    const md = nhm.translate(
+      "<h1>" + pagejson.title.rendered + "</h1>\n" + pagejson.content.rendered
+    );
+    const title = pagejson.title.rendered;
+    const titleYaml = '"' + title + '"';
+    const description = '"imported from WordPress,' + title + '"';
     const boilerplate = `--- \
     \nlayout: \"../../layouts/genericMarkdownFile.astro\" \
     \ntitle: ${titleYaml} \
     \ndescription: ${description} \
-    \n---\n\n`   
-    writeFileSync(slug + ".md", boilerplate +  md)
-}
+    \n---\n\n`;
+    let fn = slug;
+    if (i > 0) fn += `-${i}`;
+    writeFileSync(fn + ".md", boilerplate + md);
+  });
+};
 
-
-const slugData = readFileSync(process.argv[2],'utf8')
-const slugArray = slugData.split('\n')
-slugArray.forEach((slug)=>{
-    getMD(slug)
-})
+const slugData = readFileSync(process.argv[2], "utf8");
+const slugArray = slugData.split("\n");
+slugArray.forEach((slug) => {
+  getMD(slug);
+});
