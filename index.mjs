1a3be30484888d9ad51c700104913c1b66a81c8a
John R. McGarvey <johnmcgarvey55@gmail.com>
2023-05-23 21:01:18 -0400

    bugs fixed

--- index.mjs
+++ index.mjs
@@ -5,18 +5,19 @@ const  nhm =  new NodeHtmlMarkdown()
 import fetch from 'node-fetch'
 
 const getMD = async (slug) => {
-    const pagedata = await fetch("https://learn.codethedream.org/wp-json/wp/v2/pages?" + slug)
+    const pagedata = await fetch("https://learn.codethedream.org/wp-json/wp/v2/pages?" + "slug=" + slug)
     const pagejsonarray = await pagedata.json()
     const pagejson = pagejsonarray[0]
     const md = nhm.translate("<h1>" + pagejson.title.rendered + "</h1>\n" + pagejson.content.rendered)
     const title = pagejson.title.rendered
-    const description = "imported from WordPess, " + pagejson.title.rendered
+    const titleYaml = "\"" + title + "\""
+    const description = "\"imported from WordPress," + title + "\""
     const boilerplate = `--- \
     \nlayout: \"../../layouts/genericMarkdownFile.astro\" \
-    \ntitle: ${title} \
+    \ntitle: ${titleYaml} \
     \ndescription: ${description} \
     \n---\n\n`   
-    writeFileSync(slug + ".md", boilerplate + "# " + md)
+    writeFileSync(slug + ".md", boilerplate +  md)
 }
 
 
